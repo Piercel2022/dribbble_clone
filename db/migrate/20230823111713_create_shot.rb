2522803efb13923aaef301b6454ebe717a694b39
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-23 13:17:43 +0200

    Add the migration for shots

---
+++ db/migrate/20230823111713_create_shot.rb
@@ -0,0 +1,16 @@
+class CreateShot < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shots do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :image
+      t.string :title
+      t.text :description
+      t.string :tags
+      t.date :upload_date
+      t.integer :view_count
+      t.integer :likes_count
+
+      t.timestamps
+    end
+  end
+end
