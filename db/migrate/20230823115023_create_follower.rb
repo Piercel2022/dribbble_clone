8283e2db515334d0f2b7a938a12ef0cb70f5afd8
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-23 14:36:44 +0200

    Remove the deleted migration

--- db/migrate/20230823115023_create_follower.rb
+++
@@ -1,9 +0,0 @@
-class CreateFollower < ActiveRecord::Migration[7.0]
-  def change
-    create_table :followers do |t|
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
