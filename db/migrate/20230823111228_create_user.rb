9135471f2eff2eedaed48c0184e0e913b55bf1bf
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-23 13:12:59 +0200

    Add the migration for users

---
+++ db/migrate/20230823111228_create_user.rb
@@ -0,0 +1,13 @@
+class CreateUser < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_hash
+      t.string :profile_picture
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
