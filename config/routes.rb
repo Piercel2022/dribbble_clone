449f6c01871f86c3427dc7e9190b6778f8861309
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-23 16:36:40 +0200

    Add the homepage of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  #get 'homepages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homepages#index"
 end
